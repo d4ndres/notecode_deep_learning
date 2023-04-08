f9589d4d53de8c878ecb07fceb9711179c36cdd5
d4ndres <d4ndres@gmail.com>
2023-04-08 10:46:01 -0600

    2 out xor MLP

---
+++ MLP/02_xor_2outs.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from MLP import MLP
+
+x = np.array([
+[0,0], 
+[0,1], 
+[1,0], 
+[1,1] ])
+y = np.array([[1,0],[0,1],[0,1],[1,0]])
+p = 2
+nn = MLP( [p, 4, 2])
+loss = []
+epocas = 5000
+minError = 0.005
+epocasVividas = 0
+
+nn.train( x, y, epocas, minError )
+
+
+for i in range(len(y)):
+    print(x[i], ": ", nn.prediction( x[i]))
+
+plt.plot(nn.loss)
+plt.show()
